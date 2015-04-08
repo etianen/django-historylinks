1d91e004ce29d988141193b38cb37dae13f3bfcb
James Foley <jamesfoley@onespacemedia.com>
2015-04-08 10:56:50 +0100

    Moved south migrations to own folder. Added django 1.7+ migration.

--- src/historylinks/migrations/0001_initial.py
+++ src/historylinks/migrations/0001_initial.py
@@ -1,43 +1,28 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'HistoryLink'
-        db.create_table('historylinks_historylink', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('permalink_name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('permalink', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-            ('object_id', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal('historylinks', ['HistoryLink'])
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
 
-    def backwards(self, orm):
-        # Deleting model 'HistoryLink'
-        db.delete_table('historylinks_historylink')
+    operations = [
+        migrations.CreateModel(
+            name='HistoryLink',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('permalink_name', models.CharField(max_length=255)),
+                ('permalink', models.CharField(unique=True, max_length=255)),
+                ('object_id', models.TextField()),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
 
-    models = {
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'historylinks.historylink': {
-            'Meta': {'object_name': 'HistoryLink'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.TextField', [], {}),
-            'permalink': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'permalink_name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['historylinks']

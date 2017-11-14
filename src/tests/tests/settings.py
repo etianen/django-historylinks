d33b5ec615bcd01cc0182fd69ac1f93a0dcde0b5
Dave Hall <dave@etianen.com>
2017-11-14 14:01:43 +0000

    Fixing Django 1.9 and 1.8 tests

--- src/tests/tests/settings.py
+++ src/tests/tests/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     "test_historylinks",
 )
 
-MIDDLEWARE = (
+MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

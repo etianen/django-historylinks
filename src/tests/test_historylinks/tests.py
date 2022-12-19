dbc2d87918d5dc76f847dc3bfd310e67eb30c83f
Lewis Collard <lewiscollard@gmail.com>
2022-12-19 20:01:55 +0000

    Fix "buildhistorylinks" management command.
    Previously, it was relying on NoArgsCommand. This was removed in Django 1.10
    because `BaseCommand` does the same thing.
    
    While I'm there, add a test for it so it can't get quietly broken again.
    
    Driveby: `BaseCommandstdout.write` automatically adds line endings (since at
    least Django 1.8), so there's no need to do that by hand.

--- src/tests/test_historylinks/tests.py
+++ src/tests/test_historylinks/tests.py
@@ -1,7 +1,12 @@
+from io import StringIO
+
+from django.contrib.contenttypes.models import ContentType
+from django.core.management import call_command
 from django.test import TestCase
 from django.urls import reverse
 
 from historylinks import shortcuts as historylinks
+from historylinks.models import HistoryLink
 from historylinks.registration import RegistrationError
 from test_historylinks.models import HistoryLinkTestModel
 
@@ -45,6 +50,42 @@ class HistoryLinkRedirectTest(TestCase):
         # Try a 404.
         response = self.client.get("/baz/")
         self.assertEqual(response.status_code, 404)
+
+    def tearDown(self):
+        historylinks.unregister(HistoryLinkTestModel)
+
+
+class HistoryLinkManagementTestCase(TestCase):
+    def test_buildhistorylinks(self):
+        obj = HistoryLinkTestModel.objects.create(slug="foo")
+
+        def assert_historylink_is_sane():
+            self.assertEqual(HistoryLink.objects.all().count(), 1)
+            history_link = HistoryLink.objects.get()
+            self.assertEqual(history_link.content_type, ContentType.objects.get_for_model(HistoryLinkTestModel))
+            self.assertEqual(history_link.object, obj)
+            self.assertEqual(history_link.object_id, str(obj.id))
+            self.assertEqual(history_link.permalink, "/foo/")
+
+        historylinks.register(HistoryLinkTestModel)
+        # sanity check
+        self.assertEqual(HistoryLink.objects.all().count(), 0)
+
+        stdout = StringIO()
+        call_command("buildhistorylinks", stdout=stdout)
+        assert_historylink_is_sane()
+        self.assertEqual(stdout.getvalue(), "Refreshed 1 history links.\n")
+
+        # Ensure the various branches for --verbosity=X are covered.
+        stdout = StringIO()
+        call_command("buildhistorylinks", stdout=stdout, verbosity=2)
+        assert_historylink_is_sane()
+        self.assertEqual(stdout.getvalue(), "Refreshed 1 history link(s) for history link test model.\n")
+
+        stdout = StringIO()
+        call_command("buildhistorylinks", stdout=stdout, verbosity=3)
+        assert_historylink_is_sane()
+        self.assertEqual(stdout.getvalue(), f"Refreshed history link for HistoryLinkTestModel object ({obj.pk}).\n")
 
     def tearDown(self):
         historylinks.unregister(HistoryLinkTestModel)
